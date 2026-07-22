563cc762c7390064e925e5927c1f52af594a721e
WoompaLoompa <06bc1a977d@atomicmail.io>
2026-07-22 10:52:33 +0200

    feat: add Bitcoin Lightning (CLINK) plugin documentation

--- content/medusajs/plugins/_meta.js
+++ content/medusajs/plugins/_meta.js
@@ -2,6 +2,7 @@ export default {
     "get-started": "Get started",
     "available": "Available plugins",
     "automations": "Automations",
-    "notifications": "Notifications"
+    "notifications": "Notifications",
+    "bitcoin-lightning-clink": "Bitcoin Lightning (CLINK)"
   }
   
