08cde7caec946ed5acf0bf83c33f333f61c31a81
Michał Miler <michal.miler@uninterrupted.tech>
2026-09-08 08:11:22 +0200

    add fluctum to starters and plugins

--- content/medusajs/starters/_meta.js
+++ content/medusajs/starters/_meta.js
@@ -1,5 +1,6 @@
 export default {
   "get-started": "Get started",
   "available": "Available starters",
-  "astro-medusa-starter": "Astro Medusa Starter"
-}
+  "astro-medusa-starter": "Astro Medusa Starter",
+  "fluctum-starter": "Fluctum Starter",
+};
